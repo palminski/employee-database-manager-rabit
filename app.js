2fdef45750c4ae7103e9444012da0f7fc47d5a3f
palminski <williambolls98@gmail.com>
2022-09-21 09:53:33 -0700

    added all 3 tables

--- app.js
+++ app.js
@@ -11,3 +11,40 @@ const db = mysql.createConnection(
     },
     console.log('connected to database')
 );
+
+const displayEmployees = function() {
+    const sql = `SELECT * FROM employees`;
+    db.query(sql, (err, rows) => {
+        if (err) {
+            console.log('An Error has Occured');
+            return;
+        }
+        console.table(rows);
+    });
+};
+
+const displayRoles = function() {
+    const sql = `SELECT * FROM roles`;
+    db.query(sql, (err, rows) => {
+        if (err) {
+            console.log('An Error has Occured');
+            return;
+        }
+        console.table(rows);
+    });
+};
+
+const displayDepartments = function() {
+    const sql = `SELECT * FROM departments`;
+    db.query(sql, (err, rows) => {
+        if (err) {
+            console.log('An Error has Occured');
+            return;
+        }
+        console.table(rows);
+    });
+};
+
+displayEmployees();
+displayRoles();
+displayDepartments();
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,7 +1,24 @@
+INSERT INTO departments(name)
+    VALUES
+    ('Based Department'),
+    ('Finance Department'),
+    ('Computer Department');
+
+INSERT INTO roles(title,salary,department_id)
+    VALUES
+    ('CEO',999999999,1),
+    ('Bean Counter',100.5,2),
+    ('Executive Bean Counter',101,2),
+    ('nerd',69,3),
+    ('chief nerd',66.99,3),
+    ('Toadie',5,3);
+
+
 INSERT INTO employees(first_name,last_name,role_id, manager_id)
     VALUES
     ('Will', 'Bolls', 1,NULL),
-    ('Katherine', 'Yoo', 2,NULL),
-    ('Jeremy', 'Lavignino', 3,1),
+    ('Katherine', 'Yoo', 4,NULL),
+    ('Jeremy', 'Lavignino', 5,1),
     ('Caden', 'Monson', 2,1),
-    ('Josh', 'Terguson', 2,3);
+    ('Josh', 'Terguson', 6,3),
+    ('Joan', 'Bolls',3,1);
