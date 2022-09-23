a33b0909d8e8f13e69d2e5612112d2dae2568b95
palminski <107813690+palminski@users.noreply.github.com>
2022-09-23 15:22:55 -0700

    Update connections.js
    changed my password to 'password'

--- db/connections.js
+++ db/connections.js
@@ -1,10 +1,10 @@
-const password = require('../passwords/password');
+// const password = require('../passwords/password');
 const mysql = require('mysql2');
 const db = mysql.createConnection(
     {
         host: 'localhost',
         user: 'root',
-        password: password,
+        password: 'password',
         database: 'company'
     },
     console.log('connected to database')
